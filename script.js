bcdb3c7e08f7f2dd30a2c7369875a8185a632bae
MonirehBastami <monirehbastami@gmail.com>
2023-01-03 12:23:05 -0800

    Add Event Listener to Buttons; Show Winner

--- script.js
+++ script.js
@@ -1,150 +1,195 @@
 window.addEventListener('load',()=>{
-    const player1 = Math.floor(Math.random() * 6) + 1;
-    const player2 = Math.floor(Math.random() * 6) + 1;
-    const cells1 = document.querySelectorAll(".cell-p1");
-    const cells2 = document.querySelectorAll('.cell-p2');
+    
+    const dice = document.getElementById('play');
+    const editName = document.getElementById('editName');
+    const player1Name = document.querySelector('.player1Name');
+    const player2Name = document.querySelector('.player2Name');
+    const winer = document.querySelector('.winer');
+    function entername(){
+        var p1name = window.prompt("Enter the First Player Name:");
+       var p2name = window.prompt("Enter the Second Player Name:");
+       player1Name.value = p1name;
+       player2Name.value = p2name;
+       document.querySelector('#pName1').innerHTML=toCamelCase(p1name);
+       document.querySelector('#pName2').innerHTML=toCamelCase(p2name);
+    }
+    editName.addEventListener('click',()=>{
+       entername();
+    })
 
-    console.log(player1.toString());
-  
+    dice.addEventListener('click',()=>{
    
-    switch(player1.toString()){
-        case '1':
-            cells1.forEach((cell)=>{
-                console.log(cell);
-                if(cell.getAttribute('data-cell') !== '5'){ 
-                    cell.classList.add('hidden');
-                }else{
-                    cell.classList.remove('hidden');
-                    cell.classList.add('show');
-                }
-            })
-        break;
-        case '2':
-            cells1.forEach((cell)=>{
-                console.log(cell);
-                if(cell.getAttribute('data-cell') == '1' || cell.getAttribute('data-cell') == '9'){ 
-                    cell.classList.remove('hidden');
-                    cell.classList.add('show');
-                }else{
-                    cell.classList.add('hidden');
-                }
-            })
-            
-        break;
-        case '3':
-            cells1.forEach((cell)=>{
-                console.log(cell);
-                if(cell.getAttribute('data-cell') == '1' || cell.getAttribute('data-cell') == '3' || cell.getAttribute('data-cell') == '9'){ 
-                    cell.classList.remove('hidden');
-                    cell.classList.add('show');
-                }else{
-                    cell.classList.add('hidden');
-                }
-            })
-        break;
-        case '4':
-            cells1.forEach((cell)=>{
-                console.log(cell);
-                if(cell.getAttribute('data-cell') == '1' || cell.getAttribute('data-cell') == '3' || cell.getAttribute('data-cell') == '9' || cell.getAttribute('data-cell') == '7'){ 
-                    cell.classList.remove('hidden');
-                    cell.classList.add('show');
-                }else{
-                    cell.classList.add('hidden');
-                }
-            })
-        break;
-        case '5':
-            cells1.forEach((cell)=>{
-                console.log(cell);
-                if(cell.getAttribute('data-cell') == '1' || cell.getAttribute('data-cell') == '3' || cell.getAttribute('data-cell') == '5' || cell.getAttribute('data-cell') == '7' || cell.getAttribute('data-cell') == '9'){ 
-                    cell.classList.remove('hidden');
-                    cell.classList.add('show');
-                }else{
-                    cell.classList.add('hidden');
-                }
-            })
-        break;
-        case '6':
-            cells1.forEach((cell)=>{
-                console.log(cell);
-                if(cell.getAttribute('data-cell') == '1' || cell.getAttribute('data-cell') == '3' || cell.getAttribute('data-cell') == '4' || cell.getAttribute('data-cell') == '6' || cell.getAttribute('data-cell') == '7' || cell.getAttribute('data-cell') == '9'){ 
-                    cell.classList.remove('hidden');
-                    cell.classList.add('show');
-                }else{
-                    cell.classList.add('hidden');
-                }
-            })
-        break;
+        if(player1Name.value == "" || player2Name.value == ""){
+            alert('Enter the Name of Players');
+            entername();
+        }
+            const player1 = Math.floor(Math.random() * 6) + 1;
+            const player2 = Math.floor(Math.random() * 6) + 1;
+            const cells1 = document.querySelectorAll(".cell-p1");
+            const cells2 = document.querySelectorAll('.cell-p2');
+            var name1 = toCamelCase( player1Name.value);
+            var name2 = toCamelCase(player2Name.value);
 
-    }
-    switch(player2.toString()){
-        case '1':
-            cells2.forEach((cell)=>{
-                console.log(cell);
-                if(cell.getAttribute('data-cell') !== '5'){ 
-                    cell.classList.add('hidden');
-                }else{
-                    cell.classList.remove('hidden');
-                    cell.classList.add('show');
-                }
-            })
-        break;
-        case '2':
-            cells2.forEach((cell)=>{
-                console.log(cell);
-                if(cell.getAttribute('data-cell') == '1' || cell.getAttribute('data-cell') == '9'){ 
-                    cell.classList.remove('hidden');
-                    cell.classList.add('show');
-                }else{
-                    cell.classList.add('hidden');
-                }
-            })
-            
-        break;
-        case '3':
-            cells2.forEach((cell)=>{
-                console.log(cell);
-                if(cell.getAttribute('data-cell') == '1' || cell.getAttribute('data-cell') == '3' || cell.getAttribute('data-cell') == '9'){ 
-                    cell.classList.remove('hidden');
-                    cell.classList.add('show');
-                }else{
-                    cell.classList.add('hidden');
-                }
-            })
-        break;
-        case '4':
-            cells2.forEach((cell)=>{
-                console.log(cell);
-                if(cell.getAttribute('data-cell') == '1' || cell.getAttribute('data-cell') == '3' || cell.getAttribute('data-cell') == '9' || cell.getAttribute('data-cell') == '7'){ 
-                    cell.classList.remove('hidden');
-                    cell.classList.add('show');
-                }else{
-                    cell.classList.add('hidden');
-                }
-            })
-        break;
-        case '5':
-            cells2.forEach((cell)=>{
-                console.log(cell);
-                if(cell.getAttribute('data-cell') == '1' || cell.getAttribute('data-cell') == '3' || cell.getAttribute('data-cell') == '5' || cell.getAttribute('data-cell') == '7' || cell.getAttribute('data-cell') == '9'){ 
-                    cell.classList.remove('hidden');
-                    cell.classList.add('show');
-                }else{
-                    cell.classList.add('hidden');
-                }
-            })
-        break;
-        case '6':
-            cells2.forEach((cell)=>{
-                console.log(cell);
-                if(cell.getAttribute('data-cell') == '1' || cell.getAttribute('data-cell') == '3' || cell.getAttribute('data-cell') == '4' || cell.getAttribute('data-cell') == '6' || cell.getAttribute('data-cell') == '7' || cell.getAttribute('data-cell') == '9'){ 
-                    cell.classList.remove('hidden');
-                    cell.classList.add('show');
-                }else{
-                    cell.classList.add('hidden');
-                }
-            })
-        break;
-
-    }
+            console.log(player1)
+            var winerstr = player1 === player2 ? `${name1} and ${name2} WINS` : player1 > player2 ? `${name1} WINS` : `${name2} WINS`;
+            winer.classList.add('no-after');
+            winer.innerHTML = winerstr;
+            console.log(winerstr);
+            switch(player1.toString()){
+                case '1':
+                    cells1.forEach((cell)=>{
+                        // console.log(cell);
+                        if(cell.getAttribute('data-cell') == '5'){ 
+                            cell.classList.remove('hidden');
+                            cell.classList.add('show');
+                        }else{
+                            cell.classList.add('hidden');
+                            cell.classList.remove('show');
+                        }
+                    })
+                break;
+                case '2':
+                    cells1.forEach((cell)=>{
+                        //console.log(cell);
+                        if(cell.getAttribute('data-cell') == '1' || cell.getAttribute('data-cell') == '9'){ 
+                            cell.classList.remove('hidden');
+                            cell.classList.add('show');
+                        }else{
+                            cell.classList.add('hidden');
+                            cell.classList.remove('show');
+                        }
+                    })
+                    
+                break;
+                case '3':
+                    cells1.forEach((cell)=>{
+                       // console.log(cell);
+                        if(cell.getAttribute('data-cell') == '1' || cell.getAttribute('data-cell') == '3' || cell.getAttribute('data-cell') == '9'){ 
+                            cell.classList.remove('hidden');
+                            cell.classList.add('show');
+                        }else{
+                            cell.classList.add('hidden');
+                            cell.classList.remove('show');
+                        }
+                    })
+                break;
+                case '4':
+                    cells1.forEach((cell)=>{
+                        //console.log(cell);
+                        if(cell.getAttribute('data-cell') == '1' || cell.getAttribute('data-cell') == '3' || cell.getAttribute('data-cell') == '9' || cell.getAttribute('data-cell') == '7'){ 
+                            cell.classList.remove('hidden');
+                            cell.classList.add('show');
+                        }else{
+                            cell.classList.add('hidden');
+                            cell.classList.remove('show');
+                        }
+                    })
+                break;
+                case '5':
+                    cells1.forEach((cell)=>{
+                       // console.log(cell);
+                        if(cell.getAttribute('data-cell') == '1' || cell.getAttribute('data-cell') == '3' || cell.getAttribute('data-cell') == '5' || cell.getAttribute('data-cell') == '7' || cell.getAttribute('data-cell') == '9'){ 
+                            cell.classList.remove('hidden');
+                            cell.classList.add('show');
+                        }else{
+                            cell.classList.add('hidden');
+                            cell.classList.remove('show');
+                        }
+                    })
+                break;
+                case '6':
+                    cells1.forEach((cell)=>{
+                        //console.log(cell);
+                        if(cell.getAttribute('data-cell') == '1' || cell.getAttribute('data-cell') == '3' || cell.getAttribute('data-cell') == '4' || cell.getAttribute('data-cell') == '6' || cell.getAttribute('data-cell') == '7' || cell.getAttribute('data-cell') == '9'){ 
+                            cell.classList.remove('hidden');
+                            cell.classList.add('show');
+                        }else{
+                            cell.classList.add('hidden');
+                            cell.classList.remove('show');
+                        }
+                    })
+                break;
+        
+            }
+            switch(player2.toString()){
+                case '1':
+                    cells2.forEach((cell)=>{
+                       // console.log(cell);
+                        if(cell.getAttribute('data-cell') == '5'){ 
+                            cell.classList.remove('hidden');
+                            cell.classList.add('show');
+                        }else{
+                            cell.classList.add('hidden');
+                            cell.classList.remove('show');
+                        }
+                    })
+                break;
+                case '2':
+                    cells2.forEach((cell)=>{
+                       // console.log(cell);
+                        if(cell.getAttribute('data-cell') == '1' || cell.getAttribute('data-cell') == '9'){ 
+                            cell.classList.remove('hidden');
+                            cell.classList.add('show');
+                        }else{
+                            cell.classList.add('hidden');
+                            cell.classList.remove('show');
+                        }
+                    })
+                    
+                break;
+                case '3':
+                    cells2.forEach((cell)=>{
+                       // console.log(cell);
+                        if(cell.getAttribute('data-cell') == '1' || cell.getAttribute('data-cell') == '3' || cell.getAttribute('data-cell') == '9'){ 
+                            cell.classList.remove('hidden');
+                            cell.classList.add('show');
+                        }else{
+                            cell.classList.add('hidden');
+                            cell.classList.remove('show');
+                        }
+                    })
+                break;
+                case '4':
+                    cells2.forEach((cell)=>{
+                        //console.log(cell);
+                        if(cell.getAttribute('data-cell') == '1' || cell.getAttribute('data-cell') == '3' || cell.getAttribute('data-cell') == '9' || cell.getAttribute('data-cell') == '7'){ 
+                            cell.classList.remove('hidden');
+                            cell.classList.add('show');
+                        }else{
+                            cell.classList.add('hidden');
+                            cell.classList.remove('show');
+                        }
+                    })
+                break;
+                case '5':
+                    cells2.forEach((cell)=>{
+                       // console.log(cell);
+                        if(cell.getAttribute('data-cell') == '1' || cell.getAttribute('data-cell') == '3' || cell.getAttribute('data-cell') == '5' || cell.getAttribute('data-cell') == '7' || cell.getAttribute('data-cell') == '9'){ 
+                            cell.classList.remove('hidden');
+                            cell.classList.add('show');
+                        }else{
+                            cell.classList.add('hidden');
+                            cell.classList.remove('show');
+                        }
+                    })
+                break;
+                case '6':
+                    cells2.forEach((cell)=>{
+                       // console.log(cell);
+                        if(cell.getAttribute('data-cell') == '1' || cell.getAttribute('data-cell') == '3' || cell.getAttribute('data-cell') == '4' || cell.getAttribute('data-cell') == '6' || cell.getAttribute('data-cell') == '7' || cell.getAttribute('data-cell') == '9'){ 
+                            cell.classList.remove('hidden');
+                            cell.classList.add('show');
+                        }else{
+                            cell.classList.add('hidden');
+                            cell.classList.remove('show');
+                        }
+                    })
+                break;
+        
+            }
+        
+    })
 });
+function toCamelCase(str) {
+    return str.charAt(0).toUpperCase() + str.slice(1);
+}
